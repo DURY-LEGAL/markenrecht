4ae68e8089f79005913bb7f211f8c7374ad9f317
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:40:02 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// markenrecht - Markenrecht - Rechtliche Tools fuer Markenrecht und IP-Schutz. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
